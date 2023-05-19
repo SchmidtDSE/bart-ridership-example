cbec979a72b6fa69ae9c6735de4f1498b02ca9fb
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-05-18 17:38:29 -0700

    Just build sketch itself.

--- run_test.sh
+++ run_test.sh
@@ -1,29 +1,20 @@
-echo "[1 / 7] System setup"
+echo "[1 / 5] System setup"
 yes | sudo apt-get update
 yes | sudo apt-get install xvfb libxrender1 libxtst6 libxi6 default-jdk
 
-echo "[2 / 7] Loading Processing"
+echo "[2 / 5] Loading Processing"
 mkdir -p scratch
 cd scratch
 [[ ! -f processing-4.2-linux-x64.tgz ]] && wget https://github.com/processing/processing4/releases/download/processing-1292-4.2/processing-4.2-linux-x64.tgz
 tar -xf processing-4.2-linux-x64.tgz
 cd ..
 
-echo "[3 / 7] Clear prior results"
+echo "[3 / 5] Clear prior results"
 [[ -f bart_geotools/bart.png ]] && rm bart_geotools/bart.png
 
-echo "[4 / 7] Building dataset"
-cd pipeline
-bash pipeline.sh
-cd ..
-
-echo "[5 / 7] Distributing dataset..."
-rm bart_geotools/data/output.db
-cp pipeline/output.db ./bart_geotools
-
-echo "[6 / 7] Running sketch"
+echo "[4 / 5] Running sketch"
 xvfb-run ./scratch/processing-4.2/processing-java --sketch=bart_geotools --run EM
 sleep 1
 
-echo "[7 / 7] Checking results"
+echo "[5 / 5] Checking results"
 [[ ! -f bart_geotools/bart.png ]] && exit 1
