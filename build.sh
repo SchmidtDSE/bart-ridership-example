30820f383e274d3ec1a3b5551d8811478b3dcd07
Sam Pottinger <sam.pottinger@berkeley.edu>
2023-05-18 17:40:36 -0700

    Add a separate build sh file.

---
+++ build.sh
@@ -0,0 +1,29 @@
+echo "[1 / 7] System setup"
+yes | sudo apt-get update
+yes | sudo apt-get install xvfb libxrender1 libxtst6 libxi6 default-jdk
+
+echo "[2 / 7] Loading Processing"
+mkdir -p scratch
+cd scratch
+[[ ! -f processing-4.2-linux-x64.tgz ]] && wget https://github.com/processing/processing4/releases/download/processing-1292-4.2/processing-4.2-linux-x64.tgz
+tar -xf processing-4.2-linux-x64.tgz
+cd ..
+
+echo "[3 / 7] Clear prior results"
+[[ -f bart_geotools/bart.png ]] && rm bart_geotools/bart.png
+
+echo "[4 / 7] Building dataset"
+cd pipeline
+bash pipeline.sh
+cd ..
+
+echo "[5 / 7] Distributing dataset..."
+rm bart_geotools/data/output.db
+cp pipeline/output.db ./bart_geotools
+
+echo "[6 / 7] Running sketch"
+xvfb-run ./scratch/processing-4.2/processing-java --sketch=bart_geotools --run EM
+sleep 1
+
+echo "[7 / 7] Checking results"
+[[ ! -f bart_geotools/bart.png ]] && exit 1
